eb8878490dcb35ce0fba31233d126f021b8304b5
hughj095 <99343244+hughj095@users.noreply.github.com>
2023-08-08 11:52:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const apiKey = process.env.API_KEY;
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = apiKey; // Paste your API key here, referenced from github ci-cd.yml in workflows folder
+const API_KEY = ${apiKey}; // Paste your API key here, referenced from github ci-cd.yml in workflows folder
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
